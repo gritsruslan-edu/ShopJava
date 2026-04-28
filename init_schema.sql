804acd4200db59e5ebe8042a8e13ca6e3418cccb
kochuradmytro <kochura.dmytro@student.sumdu.edu.ua>
2026-04-28 17:06:49 +0300

    DB: Add db.properties with init_schema.sql

---
+++ init_schema.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(150) NOT NULL,
+    price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
+    type VARCHAR(100) NOT NULL,
+    quantity INTEGER NOT NULL DEFAULT 0 CHECK (quantity >= 0)
+);
