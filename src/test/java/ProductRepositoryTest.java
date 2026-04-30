fbc7cd30ac4c0950c738b29665106ac037d9bdf2
Vyachick228 <vladsapovalov560@gmail.com>
2026-04-30 12:34:26 +0300

    Tests: Change tests database scheme to the actual version

--- src/test/java/ProductRepositoryTest.java
+++ src/test/java/ProductRepositoryTest.java
@@ -45,12 +45,12 @@ class ProductRepositoryTest {
         ) {
             statement.execute("""
                     CREATE TABLE products (
-                        id UUID PRIMARY KEY,
+                        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                         name VARCHAR(150) NOT NULL,
-                        price DOUBLE PRECISION NOT NULL,
+                        price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
                         type VARCHAR(100) NOT NULL,
-                        quantity INTEGER NOT NULL
-                    )
+                        quantity INTEGER NOT NULL DEFAULT 0 CHECK (quantity >= 0)
+                    );
                     """);
         }
     }
